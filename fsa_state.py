7fd571b208623140e0e6e151e90924341bf7a490
Andrea Panzino <andrea.panzino@outlook.it>
2022-04-26 15:38:51 +0200

    Defined the class fsa_state.py

---
+++ fsa_state.py
@@ -0,0 +1,39 @@
+class fsaState():
+
+    def __init__(self, label=None, initial=False, final=False) -> None:
+
+        self.label = self.setLabel(label)
+        self.isInitial = self.setInitial(initial)
+        self.isFinal = self.setFinal(final)
+
+    def getLabel(self):
+
+        return self.label
+
+    def setLabel(self, label):
+
+        if (isinstance(label, str)):
+
+            self.label = label
+
+        else:
+
+            print("Incorrect data type: the label must be a string")
+
+    def getFinal(self):
+
+        return self.isFinal
+
+    def setFinal(self, isFinal):
+
+        self.isFinal = isFinal
+
+    def getInitial(self):
+
+        return self.isInitial
+
+    def setInitial(self, initial):
+
+        self.isInitial = initial
+
+    
