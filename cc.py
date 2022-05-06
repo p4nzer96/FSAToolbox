cbf0e12793e8690d196273c84de3b0798016b6cf
Dennis Loi <loidennis97@gmail.com>
2022-05-06 18:59:13 +0200

    added stepBystep option

--- cc.py
+++ cc.py
@@ -13,51 +13,51 @@ def cc(G0, G1, stepBystep=False):
     delta=[]
     while(len(Xnew)>0):
         st=Xnew[0]
-        print("Stato: ("+st[0].label+","+st[1].label+")")
+        if(stepBystep): print("Stato: ("+st[0].label+","+st[1].label+")")
         for event in CC.E:
-            print("Evento: "+event.label, end='->')
+            if(stepBystep): print("Evento: "+event.label, end='->')
             if(event in G0.E and event not in G1.E): #private to G0
-                print("Private G0", end='->')
+                if(stepBystep): print("Private G0", end='->')
                 trans=G0.filter_delta(start=str(st[0].label),transition=str(event.label))
                 if (not trans.empty):
                     for inx,el in trans.iterrows():
-                        print(el['start'].label+" "+el['transition'].label+" "+el['end'].label)
+                        if(stepBystep): print(el['start'].label+" "+el['transition'].label+" "+el['end'].label)
                         newState=[el['end'],st[1]]
                         delta.append([st,event,newState])
                         if(newState not in X and newState not in Xnew):
                             Xnew.append(newState)
                 else:
-                    print("No transitions")
+                    if(stepBystep): print("No transitions")
                     
             elif(event not in G0.E and event in G1.E): #private to G1
-                print("Private G1", end='->')
+                if(stepBystep): print("Private G1", end='->')
                 trans=G1.filter_delta(start=str(st[1].label),transition=str(event.label))
                 if (not trans.empty):
                     for inx,el in trans.iterrows():
-                        print(el['start'].label+" "+el['transition'].label+" "+el['end'].label)
+                        if(stepBystep): print(el['start'].label+" "+el['transition'].label+" "+el['end'].label)
                         newState=[st[0],el['end']]
                         delta.append([st,event,newState])
                         if(newState not in X and newState not in Xnew):
                             Xnew.append(newState)
                 else:
-                    print("No transitions")
+                    if(stepBystep): print("No transitions")
                     
             else: #synchronized
-                print("Synchronized", end='->')
+                if(stepBystep): print("Synchronized", end='->')
                 transG0=G0.filter_delta(start=str(st[0].label),transition=str(event.label))
                 transG1=G1.filter_delta(start=str(st[1].label),transition=str(event.label))
                 
                 if(not transG0.empty and not transG1.empty):
                     for inx,el0 in transG0.iterrows():
                         for jnx,el1 in transG1.iterrows():
-                            print("["+el0['start'].label+" "+el0['transition'].label+" "+el0['end'].label,end=" , ")
-                            print(el1['start'].label+" "+el1['transition'].label+" "+el1['end'].label+"]")
+                            if(stepBystep): print("["+el0['start'].label+" "+el0['transition'].label+" "+el0['end'].label,end=" , ")
+                            if(stepBystep): print(el1['start'].label+" "+el1['transition'].label+" "+el1['end'].label+"]")
                             newState=[el0['end'],el1['end']]
                             delta.append([st,event,newState])
                             if(newState not in X and newState not in Xnew):
                                 Xnew.append(newState)
                 else:
-                    print("No transitions")
+                    if(stepBystep): print("No transitions")
                 
         X.append(st)
         Xnew.remove(st)
