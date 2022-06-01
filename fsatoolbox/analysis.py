96146d7f98480bc59fa52af4dae4b40d8ace70ab
Dennis Loi <loidennis97@gmail.com>
2022-06-01 11:13:20 +0200

    bozze di analysis and loadfsa

---
+++ fsatoolbox/analysis.py
@@ -0,0 +1,4 @@
+def reachability
+
+def co-reach
+
---
+++ fsatoolbox/loadfsa.py
@@ -0,0 +1,22 @@
+
+from fsatoolbox import *
+
+
+def loadfsa(path):
+
+    #check estensione
+    
+    #crea oggetto fsa
+
+    #"carica" su l'oggetto
+
+    #restituisce l'oggetto
+    return a
+
+
+
+'''
+G0 = fsa.fromfile()
+G0 = loadfsa("C:\")
+G0.add_state...
+'''
