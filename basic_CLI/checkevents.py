81d489316d43424c0cf943788686a1236599b87d
Dennis Loi <loidennis97@gmail.com>
2022-06-10 16:32:00 +0200

    cli: events pool functions

---
+++ basic_CLI/checkevents.py
@@ -0,0 +1,29 @@
+import fsatoolbox
+from fsatoolbox import *
+
+def updateevents(eventslst, fsalst):
+    for e in eventslst:
+        for G in fsalst:
+            pass
+            #G.change_event_prop(e.label, isObservable=e.isObservable, isControllable=e.isControllable, isFault=e.isFault)
+
+def checkevents(G, eventslst, fsalst):
+    for e in G.E:
+        if e not in eventslst:
+            eventslst.append(e)
+        else:
+            ex_e = [i for i in eventslst if i.label == e.label][0]
+            if(e.isObservable != ex_e.isObservable or e.isControllable != ex_e.isControllable or e.isFault != ex_e.isFault):
+                print("Two events with the label \""+e.label+"\" have different proprieties, which one do you want to keep?")
+                print("1) Observable: "+str(e.isObservable)+", Controllable: "+str(e.isControllable)+", Fault: "+str(e.isFault))
+                print("2) Observable: "+str(ex_e.isObservable)+", Controllable: "+str(ex_e.isControllable)+", Fault: "+str(ex_e.isFault))
+                inp = input(">")
+                if inp=='2':
+                    #keep the event proprieties from the pool
+                    return
+                else:
+                    #update event proprieties
+                    ex_e.isObservable=e.isObservable
+                    ex_e.isControllable=e.isControllable
+                    ex_e.isFault=e.isFault
+    updateevents(eventslst, fsalst)
