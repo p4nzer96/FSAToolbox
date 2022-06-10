45e77b4eb79c7ddcb3914871d13b5070ea71b83f
Dennis Loi <loidennis97@gmail.com>
2022-06-10 12:20:03 +0200

    Diagnoser first commit

---
+++ fsatoolbox/diag.py
@@ -0,0 +1,25 @@
+from fsatoolbox import *
+from fsatoolbox.utils.misc import v_print
+
+def diag(G, verbose=False):
+
+    #Fault monitor
+    v_print("Compute the fault monitor:", verbose)
+    FM=fm(G, verbose)
+    v_print("\n\nFault monitor:",verbose)
+    v_print(FM,verbose)
+
+    #Fault recognizer
+    v_print("Compute the fault recognizer:", verbose)
+    FR=cc(G,FM,verbose)
+    v_print("\n\nFault recognizer:",verbose)
+    v_print(FR,verbose)
+
+    #Diagnoser
+    v_print("Compute the diagnoser:", verbose)
+    D=nfa2dfa(FR, verbose=verbose)
+    v_print("\n\nDiagnoser:",verbose)
+    v_print(FM,verbose)
+
+    return D
+
