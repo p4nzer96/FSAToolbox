08dbe642812aa7d9b872012087502cc83750e2d4
Dennis Loi <loidennis97@gmail.com>
2022-06-10 16:33:32 +0200

    cli: fsabuilder added events pool check

--- basic_CLI/fsabuilder.py
+++ basic_CLI/fsabuilder.py
@@ -1,5 +1,6 @@
 import fsatoolbox
 from fsatoolbox import *
+from basic_CLI.checkevents import checkevents
 
 #TODO add exit (abort) option in every input
 
@@ -104,5 +105,7 @@ def fsabuilder(args, fsalst, path):
                 G.add_transition(inp[0], inp[1], inp[2])
             except:
                 pass
+
+    checkevents(G,eventslst, fsalst)
 
     fsalst[args[0]]=G
