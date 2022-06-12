c872755ace915107e4a4ed4cd38a40f3dd435f0d
Andrea Panzino <andrea.panzino@outlook.it>
2022-06-12 21:49:28 +0200

    basicCLI: improved help (v 0.5)

--- basicCLI.py
+++ basicCLI.py
@@ -13,91 +13,156 @@ from basic_CLI.diagnoser import diagnoser
 from basic_CLI.observer import observer
 from basic_CLI.super import supervisor
 from basic_CLI.exth import exth
+from termcolor import colored
 
 from basic_CLI.analysis import reachability, coreachability, blocking, trim, dead, reverse
 
-#commands
 
-def help(args,eventslst,fsalst,path):
-    print("This is only a test version: available commands:")
-    for key,val in commands.items():
-        print("-> "+key)
-    print("[CTRL+C to exit]")
+# commands
+
+def help(args, eventslst, fsalst, path):
+    print("This is only a test version: available commands:\n")
+
+    print(colored("-------------------------- Basic Commands  --------------------------", "green"))
+    print("Commands used to load / save an FSA from / to file\n")
+
+    print("-> " + colored("changepath", "yellow") + ":\tChanges the current working directory")
+    print("-> " + colored("path", "yellow") + ":     \tShows the current working directory")
+    print("-> " + colored("load", "yellow") + ":     \tLoads a FSA from a file")
+    print("-> " + colored("save", "yellow") + ":     \tSaves a FSA to a file")
+    print("-> " + colored("build", "yellow") + ":     \tCalls a wizard to build the FSA")
+    print("-> " + colored("show", "yellow") + ":     \tShow a FSA")
+    print("-> " + colored("list", "yellow") + ":     \tLists all FSAs")
+
+    print(colored("\n-------------------------- Edit FSA --------------------------", "green"))
+    print("Commands used to edit a FSA\n")
+
+    print("-> " + colored("addstate", "yellow") + ":\tAdds a state to a FSA")
+    print("-> " + colored("rmstate", "yellow") + ": \tRemoves a state from a FSA")
+    print("-> " + colored("addevent", "yellow") + ":\tAdds an event to a FSA")
+    print("-> " + colored("rmevent", "yellow") + ": \tRemoves an event from a FSA")
+    print("-> " + colored("elist", "yellow") + ":   \tLists all events of a FSA")
+    print("-> " + colored("editevent", "yellow") + "\tEdits event properties")
+    print("-> " + colored("addtrans", "yellow") + ":\tAdds a transition to a FSA")
+    print("-> " + colored("rmtrans", "yellow") + ": \tRemoves a transition to a FSA")
+
+    print(colored("\n-------------------------- FSA Functions --------------------------", "green"))
+    print("Commands used to call functions on FSAs\n")
+
+    print("-> " + colored("cc", "yellow") + ":       \tComputes the concurrent composition between two FSAs")
+    print("-> " + colored("fm", "yellow") + ":       \tComputes the Fault Monitor of a FSA")
+    print("-> " + colored("diag", "yellow") + ":     \tComputes the diagnoser of a FSA")
+    print("-> " + colored("nfa2dfa", "yellow") + ":    \tConverts a NFA into a DFA")
+    print("-> " + colored("supervisor", "yellow") + ":\tComputes the supervisor of a FSA")
+
+    print(colored("\n-------------------------- FSA Analysis --------------------------", "green"))
+    print("Functions for analyze a FSA\n")
+
+    print("-> " + colored("reach", "yellow") + ":\tComputes the reachability of a FSA")
+    print("-> " + colored("coreach", "yellow")+ ":\tComputes the co-reachability of a FSA")
+    print("-> " + colored("blocking", "yellow")+ ":\tComputes if a FSA is blocking")
+    print("-> " + colored("trim", "yellow")+ ":\tComputes if a FSA is trim")
+    print("-> " + colored("dead", "yellow")+ ":\tComputes the dead states of a FSA")
+    print("-> " + colored("reverse", "yellow")+ ":\tComputes if a FSA is reversbible")
+
+    print(colored("\n[CTRL+C or exit to quit the program]\n", "red"))
+
 
 def changepath(args, path):
-    if('-h' in args):
-        print("This functions changes the default path")
-        print("Usage:\n     changepath newpath (Ex: changepath C:\\\\Automi)")
-        print("Note: In windows use \\\\ instead of \\ (C:\\\\Automi) or put the path in brakets: \"C:\Automi\"")
-        return path
-    
-    if(len(args)<1):
-        print("Not enough arguments provided, type \"changepath -h\" to help")
+    if '-h' in args:
+        print(colored("\nchangepath: ", "yellow", attrs=["bold"]) + "This functions changes the default path\n")
+        print(colored("Usage:", attrs=["bold"]) + "\n\tchangepath newpath")
+        print(colored("Example:", attrs=["bold"]) + "\n\tchangepath C:\\\\Automi")
+        print(colored("Notes: ", attrs=["bold"]) + "\n\t * In windows use \\\\ instead of \\ (ex. C:\\\\Automi) or "
+                                                   "put the path in brackets (ex. \"C:\\Automi\\\")")
         return path
 
-    if(os.path.isdir(args[0])):
+    if len(args) < 1:
+        print(colored("Not enough arguments provided, type \"changepath -h\" to help", "red"))
+        return path
+
+    if os.path.isdir(args[0]):
         return args[0]
     else:
-        print("Invalid path")
+        print(colored("Invalid path", "red"))
         return path
 
-def removefsa(args,eventslst,fsalst,path):
-    if('-h' in args):
-        print("Removes a fsa from the list")
-        print("Usage:\n->rm fsa_name (Ex:rm G0)")
+
+def removefsa(args, eventslst, fsalst, path):
+    if '-h' in args:
+        print(colored("\nremove:", "yellow", attrs=["bold"]) + " Removes a FSA\n")
+        print(colored("Usage:\n\t", attrs=["bold"]) + "remove fsa_name")
+        print(colored("Example:\n\t", attrs=["bold"]) + "remove G0")
         return
 
-    if(len(args)<1):
+    if len(args) < 1:
         print("Not enough arguments provided, type \"rm -h\" to help")
         return
 
-    if(args[0] not in fsalst):
+    if args[0] not in fsalst:
         print("fsa not found")
         return
 
     del fsalst[args[0]]
 
-def currpath(args,eventslst,fsalst,path):
-    if('-h' in args):
-        print("Print the current path")
 
-    print(path)
+def currpath(args, eventslst, fsalst, path):
+    if '-h' in args:
+        print(colored("path:", "yellow") + "Prints the current working directory")
+    else:
+        print(path)
 
-def showfsa(args,eventslst,fsalst,path):
-    #TODO
-    if(len(args)<1):
-        print("Not enough arguments provided")
+
+def showfsa(args, eventslst, fsalst, path):
+
+    if '-h' in args:
+        print(colored("\nshow: ", "yellow", attrs=["bold"]) + "Prints the structure of the FSA\n")
+        print(colored("Usage:", attrs=["bold"]) + "\n\tshow fsa_name")
+        print(colored("Example:", attrs=["bold"]) + "\n\tshow G0")
+
+    if len(args) < 1:
+        print(colored("Not enough arguments provided", "red"))
         return
 
-    if(args[0] not in fsalst):
-        print("Error, fsa doesn't exists")
+    if args[0] not in fsalst:
+        print(colored("Error, fsa doesn't exists", "red"))
         return
-    
+
     print(fsalst[args[0]])
 
-def lst(args,eventslst,fsalst,path):
-    print("Elements in: " + path+"\\")
-    l = os.listdir(path+"\\")  # files only
+
+def lst(args, eventslst, fsalst, path):
+    print("Elements in: " + path + "\\")
+    l = os.listdir(path + "\\")  # files only
     for el in l:
         print(el)
 
-def listfsa(args,eventslst,fsalst,path): #TODO add some stats?
-    for key,value in fsalst.items():
+
+def listfsa(args, eventslst, fsalst, path):  # TODO add some stats?
+
+    if '-h' in args:
+        print(colored("\nload: ", "yellow", attrs=["bold"]) + "Prints the FSAs currently loaded\n")
+        print(colored("Usage:", attrs=["bold"]) + "\n\tlist")
+
+    for key, value in fsalst.items():
         print(key)
+
 
 def listevents(args, eventslst, fsalst, path):
     for e in eventslst:
-        print("- "+e.label+":  Observable: "+str(e.isObservable)+", Controllable: "+str(e.isControllable)+", Fault: "+str(e.isFault))
+        print("- " + e.label + ":  Observable: " + str(e.isObservable) + ", Controllable: " + str(
+            e.isControllable) + ", Fault: " + str(e.isFault))
+
 
 def editevent(args, eventslst, fsalst, path):
-    if ('-h' in args):
+    if '-h' in args:
         print("This function is used to edit an event that is loaded in the event list")
         print("Usage:\n->editevent event-name -options")
         print("-o set the event as observable")
         print("-c set the event as controllable")
         print("-f set the event as a fault event")
         return
-    if(len(args)<1):
+    if len(args) < 1:
         print("Not enough arguments provided, type \"editevent -h\" to help")
         return
 
@@ -106,55 +171,56 @@ def editevent(args, eventslst, fsalst, path):
         return
 
     e = [i for i in eventslst if i.label == args[0]][0]
-    e.isObservable=('-o' in args)
-    e.isControllable=('-c' in args)
-    e.isFault=('-f' in args)
+    e.isObservable = ('-o' in args)
+    e.isControllable = ('-c' in args)
+    e.isFault = ('-f' in args)
 
-    print("- "+e.label+":  Observable: "+str(e.isObservable)+", Controllable: "+str(e.isControllable)+", Fault: "+str(e.isFault))
+    print("- " + e.label + ":  Observable: " + str(e.isObservable) + ", Controllable: " + str(
+        e.isControllable) + ", Fault: " + str(e.isFault))
 
     updateevents(eventslst, fsalst)
 
-#list of loaded FSA
-fsalst=dict()
-eventslst=[]
 
-commands={
-    'changepath' :  changepath,
-    'path' :        currpath,
-    'load':         loadfsa,
-    'rm':           removefsa,
-    'save':         savefsa,
-    'build':        fsabuilder,
-    'addstate':     addstate,
-    'rmstate':      rmstate,
-    'addevent' :    addevent,
-    'rmevent' :     rmevent,
-    'addtrans' :    addtrans,
-    'rmtrans' :     rmtrans,
-    'show':         showfsa,
-    'reach' :       reachability,
-    'coreach' :     coreachability,
-    'blocking' :    blocking,
-    'trim' :        trim,
-    'dead' :        dead,
-    'reverse' :     reverse,
-    'lst' :         lst,
-    'xlist':        listfsa,
-    'elist':        listevents,
-    'editevent' :   editevent,
-    'cc':           conccomp,
-    'fm':           faultmon,
-    'diag' :        diagnoser,
-    'nfa2dfa':      observer,
-    'obs':          observer,
-    'supervisor':   supervisor,
-    'exth':         exth,
-    'help':         help
+# list of loaded FSA
+fsalst = dict()
+eventslst = []
+
+commands = {
+    'changepath': changepath,
+    'path': currpath,
+    'load': loadfsa,
+    'remove': removefsa,
+    'save': savefsa,
+    'build': fsabuilder,
+    'addstate': addstate,
+    'rmstate': rmstate,
+    'addevent': addevent,
+    'rmevent': rmevent,
+    'addtrans': addtrans,
+    'rmtrans': rmtrans,
+    'show': showfsa,
+    'reach': reachability,
+    'coreach': coreachability,
+    'blocking': blocking,
+    'trim': trim,
+    'dead': dead,
+    'reverse': reverse,
+    'lst': lst,
+    'list': listfsa,
+    'elist': listevents,
+    'editevent': editevent,
+    'cc': conccomp,
+    'fm': faultmon,
+    'diag': diagnoser,
+    'nfa2dfa': observer,
+    'obs': observer,
+    'supervisor': supervisor,
+    'exth': exth,
+    'help': help
 }
 
-
-home=os.path.expanduser("~")
-path=home+'\\Documents\\FsaToolbox'
+home = os.path.normpath(os.path.expanduser("~"))
+path = home + '/Documents/FsaToolbox'
 
 if not os.path.exists(path):
     os.makedirs(path)
@@ -173,22 +239,18 @@ print("\n\nNote: the default path is:")
 print(path)
 print("")
 
-
-while(1):
-    cmd=shlex.split(input(">>"))
-    if cmd==[]:
+while 1:
+    cmd = shlex.split(input(">>"))
+    if not cmd:
         continue
-    args = cmd[1:] #extract arguments
+    args = cmd[1:]  # extract arguments
 
-    if(cmd[0]=='changepath'):
-        path=changepath(args, path)
+    if cmd[0] == 'changepath':
+        path = changepath(args, path)
         continue
-    if(cmd[0]=='exit'):
+    if cmd[0] == 'exit':
         break
-    if(cmd[0] in commands):
-        commands[cmd[0]](args,eventslst,fsalst,path)
+    if cmd[0] in commands:
+        commands[cmd[0]](args, eventslst, fsalst, path)
     else:
-        print("unrecognized command")
-
-
-
+        print(colored("Uanrecognized command", "red"))
