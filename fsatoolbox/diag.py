9ee379fff13345e503ec87c19dcdad50daf23eca
Dennis Loi <loidennis97@gmail.com>
2022-07-06 15:55:18 +0200

    better print in verbose mode

--- fsatoolbox/diag.py
+++ fsatoolbox/diag.py
@@ -5,21 +5,18 @@ from fsatoolbox.utils.misc import v_print
 
 def diag(G, verbose=False):
     # Fault monitor
-    v_print("Compute the fault monitor:", verbose)
-    FM = fm(G, verbose)
+    FM = fm(G)
     v_print("\n\nFault monitor:", verbose)
     v_print(FM, verbose)
 
     # Fault recognizer
-    v_print("Compute the fault recognizer:", verbose)
-    FR = cc(G, FM, verbose)
+    FR = cc(G, FM)
     v_print("\n\nFault recognizer:", verbose)
     v_print(FR, verbose)
 
     # Diagnoser
-    v_print("Compute the diagnoser:", verbose)
-    D = nfa2dfa(FR, verbose=verbose)
+    D = nfa2dfa(FR)
     v_print("\n\nDiagnoser:", verbose)
-    v_print(FM, verbose)
+    v_print(D, verbose)
 
     return D
