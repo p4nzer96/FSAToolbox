69077222dbdbc9d2e26a1dca7ce7f9345ac46e93
Dennis Loi <loidennis97@gmail.com>
2022-07-06 15:59:11 +0200

    added exception handling

--- fsatoolbox_cli/command_basic.py
+++ fsatoolbox_cli/command_basic.py
@@ -12,19 +12,28 @@ class command_basic(command):
 
     def func_call(self, args: list, opts: list):
 
+        # Check if I have to print the help
         if "-h" in opts:
             self.helper()
             return
 
+        # (ERROR) Too few arguments
         if len(args) < min(self.n_req_args):
             print(colored("Not enough arguments provided, type \"{} -h\" to help", self.WARN_COLOR).format(self.f_name))
 
+        # Correct number of arguments
         elif len(args) in self.n_req_args:
 
-            if "fsa_dict" in inspect.signature(self.callback).parameters:
-                self.callback(*args, fsa_dict=self.fsa_dict)
-            else:
-                self.callback(*args)
+            try:
+                if "fsa_dict" in inspect.signature(self.callback).parameters:
+                    self.callback(*args, fsa_dict=self.fsa_dict)
+                else:
+                    self.callback(*args)
+            except Exception as e:
+                print(colored("There was an error:", "red"))
+                print(e)
+                return
 
+        # (ERROR) Too many arguments
         else:
-            print(colored("Too much arguments provided, type \"{} -h\" to help", self.WARN_COLOR).format(self.f_name))
+            print(colored("Too many arguments provided, type \"{} -h\" to help", self.WARN_COLOR).format(self.f_name))
