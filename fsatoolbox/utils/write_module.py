13c33e0f24d184413b02758338716c8f34fe7b79
Dennis Loi <loidennis97@gmail.com>
2022-07-06 15:57:04 +0200

    added default value if event propriety not set

--- fsatoolbox/utils/write_module.py
+++ fsatoolbox/utils/write_module.py
@@ -68,21 +68,21 @@ def save_txt(filename, X, E, delta):
             elif event.isControllable is False:
                 controllable = "uc"
             else:
-                controllable = ""
+                controllable = "c" #default fallback
 
             if event.isObservable is True:
                 observable = "o"
             elif event.isObservable is False:
                 observable = "uo"
             else:
-                observable = ""
+                observable = "o" #default fallback
 
             if event.isFault is True:
                 fault = "f"
             elif event.isFault is False:
                 fault = "uf"
             else:
-                fault = ""
+                fault = "uf" #default fallback
 
             event_dict[event] = {"isControllable": controllable, "isObservable": observable, "isFault": fault}
 
